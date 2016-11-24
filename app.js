b7b61fe0ff29092e67ee4966a2b19e5f09e97c2b
Corw <pzamyatin@gmail.com>
2016-11-24 17:13:01 +1300

    Added http module

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+// add http module
 const http = require('http');
+const fs = require('fs');
+
+const myReadStream = fs.createReadStream(__dirname + '/readMe', 'utf8');
+
+myReadStream.on('data', function(chunk) {
+  console.log('new chunk received:');
+  console.log(chunk);
+})
+
+
 
 // create NodeJS server
-const server = http.createServer(function(request, respond) {
-
-});
+// const server = http.createServer(function(req, res) {
+//   console.log('request was made: ' + req.url);
+//
+//   // create response - header and message
+//   res.writeHead(200, {'Content-Type' : 'text/plain'});
+//   res.end('It works!');
+//
+// });
+//
+// // listen to 3000 port and ip-adress
+// server.listen(3000, '127.0.0.1');
+// console.log('Now we are listening port 3000...');
